592576a466df0feb30162febc1413fd24003672c
volodymyr99 <492145768@ukr.net>
2023-06-29 08:35:06 +0300

    add phpunit tests

---
+++ tests/PostTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+//include_once "c:\openserver\domains\blog\classes\Post.php";
+
+class PostTest extends TestCase
+{
+    private $post;
+
+    protected function setUp(): void
+    {
+        // Create a mock of the QueryBuilder class
+        $this->post = $this->getMockBuilder(Post::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+    }
+
+    public function testCreatePost()
+    {
+        $_POST['title'] = 'Test Title';
+        $_POST['description'] = 'Test Description';
+
+        // Mock the database connection and query
+        $mockQuery = $this->getMockBuilder(PDOStatement::class)
+            ->getMock();
+        $mockQuery->expects($this->once())
+            ->method('execute')
+            ->with([$this->equalTo('Test Title'), $this->equalTo('Test Description'), $this->anything(), $this->anything()])
+            ->willReturn(true);
+        $this->post->db = $this->getMockBuilder(PDO::class)
+            ->getMock();
+        $this->post->db->expects($this->once())
+            ->method('prepare')
+            ->with('INSERT INTO posts VALUES(NULL,?,?,?,?)')
+            ->willReturn($mockQuery);
+
+        $this->post->createPost(123);
+
+        $this->assertTrue($this->post->successPost);
+    }
+
+    public function testGetAll()
+    {
+        // Mock the database connection and query
+        $mockQuery = $this->getMockBuilder(PDOStatement::class)
+            ->getMock();
+        $mockQuery->expects($this->once())
+            ->method('fetchAll')
+            ->with(PDO::FETCH_OBJ)
+            ->willReturn(['post1', 'post2']); // Replace with actual data
+        $this->post->db = $this->getMockBuilder(PDO::class)
+            ->getMock();
+        $this->post->db->expects($this->once())
+            ->method('prepare')
+            ->with('SELECT * from posts')
+            ->willReturn($mockQuery);
+
+        $result = $this->post->getAll();
+
+        $this->assertEquals(['post1', 'post2'], $result);
+    }
+
+    // Add more test methods for the remaining functions...
+
+    // For example:
+    // public function testGetUser()
+    // public function testDeletePost()
+    // public function testGetAllUserPosts()
+    // public function testGetSinglePost()
+    // public function testGetAllComments()
+    // public function testAddComment()
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+//set_include_path("c:\openserver\domains\blog\classes");
+//include_once "QueryBuilder.php";
+//include_once "User.php";
+
+
+
+class UserTest extends TestCase
+{
+    protected $user;
+
+    protected function setUp(): void
+    {
+        $this->user = new User(); // Create an instance of the User class for each test
+        // You may need to initialize any dependencies required by the User class
+    }
+
+    public function testRegisterUserSuccess()
+    {
+        // Simulate a POST request with the required data
+        $_POST['name'] = 'John Doe';
+        $_POST['register_email'] = 'john@example.com';
+        $_POST['register_password'] = 'password123';
+
+        // You may need to mock the database connection and the query execution
+        $mockDb = $this->createMock(PDO::class);
+        $this->user->setDb($mockDb);
+
+        $mockQuery = $this->createMock(PDOStatement::class);
+        $mockQuery->method('execute')->willReturn(true);
+        $mockDb->method('prepare')->willReturn($mockQuery);
+
+        $this->user->registerUser();
+
+        // Assert that the successRegister property is true
+        $this->assertTrue($this->user->successRegister);
+    }
+
+    // Add more test methods for other scenarios (e.g., failed registration, login, and password reset)
+
+    protected function tearDown(): void
+    {
+        $this->user = null; // Release the instance of the User class after each test
+    }
+}
